17503c2f6264dd4eae2a697609f03d64346c80e4
Emma Malysz <emalysz@mozilla.com>
2023-06-06 11:55:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+console.log("In index.js");
+const fs = require('fs');
+fs.mkdirSync("./logging");
+fs.appendFileSync('./logging/logfile.txt', 'Got here!');
+
+
 function helloWorld() {
   console.log('Hello World from this npm package');
 }
