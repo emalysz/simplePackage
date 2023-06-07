1d40c0adc305cb02778e769d81b2c132d91198ab
Emma Malysz <emalysz@mozilla.com>
2023-06-07 10:33:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-console.log("In index.js");
 const fs = require('fs');
-fs.mkdirSync("./logging");
 fs.appendFileSync('./logging/logfile.txt', 'Got here!');
+
+module.exports.plugin = (on, config) => {
+    fs.appendFileSync('./logging/logfile.txt', 'module.exports.plugin');
+    return config;
+};
+
+module.exports.lifecycle = () => {
+  fs.appendFileSync('./logging/logfile.txt', 'module.exports.lifecycle');
+};
 
 
 function helloWorld() {
