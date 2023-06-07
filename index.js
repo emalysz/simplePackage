24bf498f60f6e3767133ffc34d60de639e0f5db8
Emma Malysz <emalysz@mozilla.com>
2023-06-07 12:10:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const fs = require('fs');
 fs.appendFileSync('./logging/logfile.txt', 'Got here!');
 
-module.exports.plugin = (on, config) => {
-    fs.appendFileSync('./logging/logfile.txt', 'module.exports.plugin');
-    return config;
-};
-
-module.exports.lifecycle = () => {
-    console.log("do we ever get here?");
-  fs.appendFileSync('./logging/logfile.txt', 'module.exports.lifecycle');
-};
-
-
-function helloWorld() {
-  console.log('Hello World from this npm package');
-    fs.appendFileSync('./logging/logfile.txt', 'helloWorld()');
+function getGlobals() {
+	console.log("***************WILL PRINT*************");
 }
 
-module.exports = helloWorld;
+module.exports.plugin = (on, config) => {
+    getGlobals();
+    fs.appendFileSync('./logging/logfile.txt', 'WILL NOT PRINT');
+    getGlobals();
+    return config;
+};
